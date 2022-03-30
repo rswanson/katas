cdc7d51e24d0fcbafdbc392d8894f8bfb3fb7ffd
Ryan Swanson <ryan.s.swanson@gmail.com>
2022-03-30 17:46:41 -0400

    re-initializing card-game-war as a node app

---
+++ packages/card-game-war/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'card-game-war',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/packages/card-game-war',
+};
