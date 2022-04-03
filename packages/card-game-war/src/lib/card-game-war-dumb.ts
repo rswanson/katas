866dc1556d62e8f78d9f1c8ef0f5232850f3c2a6
Ryan Swanson <ryan.s.swanson@gmail.com>
2022-04-02 21:42:30 -0400

    implemented card type, deck is WIP, bad me not working code but i am tired and this is my repo

--- packages/card-game-war/src/lib/card-game-war-dumb.ts
+++ packages/card-game-war/src/lib/card-game-war-dumb.ts
@@ -2,13 +2,53 @@ import { Random, MersenneTwister19937 } from "random-js";
 const random = new Random(MersenneTwister19937.autoSeed());
 
 
-type Card = {
-  name: string;
+enum Suit {
+  Clubs,
+  Diamonds,
+  Hearts,
+  Spades,
+}
+type CardType = {
+  // name: string;
+  suit: Suit;
   value: number;
+}
+
+type DeckType = {
+  cards: Array<CardType>;
+  currentPosition: number
+}
+export class Card implements CardType {
+  suit: Suit;
+  value: number;
+  constructor(suit: Suit, value: number) {
+    // this.name = name;
+    this.suit = suit;
+    this.value = value;
+  }
+}
+export class Deck implements DeckType {
+  cards: Array<Card>;
+  for (let suit = 0; suit < 4; suit++) {
+    for (let value = 2; value < 14; value++) {
+      if (suit == 0)
+        this.cards[value-2] = new Card(Suit["Clubs"],value)
+      if (suit == 1)
+        this.cards[value-2] = new Card(Suit["Diamonds"],value)
+      if (suit == 2)
+        this.cards[value-2] = new Card(Suit["Hearts"],value)
+      if (suit == 3)
+        this.cards[value-2] = new Card(Suit["Spades"],value)
+    }
+  }
 };
 
-export function draw(): number {
-  return random.integer(2, 14);
+// export function draw(deck: Deck, position: number): Card {
+//   return deck[position];
+// }
+
+export function doesMyCardWin(myCard: Card, notMyCard: Card): boolean {
+  return myCard["value"] > notMyCard["value"];
 }
 
 export function cardGameWarDumb(): string {
