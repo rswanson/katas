80652d851259c5c0f1618e7341261093bc1a409b
Ryan Swanson <ryan.s.swanson@gmail.com>
2022-04-02 20:30:32 -0400

    initial dumb dumb tests

--- packages/card-game-war/src/lib/card-game-war-dumb.ts
+++ packages/card-game-war/src/lib/card-game-war-dumb.ts
@@ -1,9 +1,17 @@
+import { Random, MersenneTwister19937 } from "random-js";
+const random = new Random(MersenneTwister19937.autoSeed());
+
+
 type Card = {
   name: string;
   value: number;
+};
+
+export function draw(): number {
+  return random.integer(2, 14);
 }
 
 export function cardGameWarDumb(): string {
+  console.log( draw())
   return 'card-game-war';
 }
-
--- packages/card-game-war/src/lib/card-game-war.spec-dumb.ts
+++
@@ -1,7 +0,0 @@
-import { cardGameWarDumb } from './card-game-war-dumb';
-
-describe('bigger card wins', () => {
-  it('should work', () => {
-    expect(cardGameWarDumb()).toEqual('card-game-war');
-  });
-});
