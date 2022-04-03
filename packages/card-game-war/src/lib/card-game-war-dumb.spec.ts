efa3882e45981c569e052c03c7a3d3ce5d7495aa
Ryan Swanson <ryan.s.swanson@gmail.com>
2022-04-03 11:47:31 -0400

    removing commented code and uncommenting deck length check

--- packages/card-game-war/src/lib/card-game-war-dumb.spec.ts
+++ packages/card-game-war/src/lib/card-game-war-dumb.spec.ts
@@ -6,14 +6,9 @@ describe('the basics', () => {
   it('should work', () => {
     expect(cardGameWarDumb()).toEqual('card-game-war');
   });
-  // it('should give a number between 2 and 14 (inclusive)', ()=> {
-  //   const drawResult = draw()
-  //   expect(drawResult >= 2)
-  //   expect(drawResult <= 14)
-  // });
   it('should initialize a new 52 card deck without shuffling', () => {
     const deck = new Deck();
-    // expect(deck.cards.length).toBe(52);
+    expect(deck.cards.length).toBe(52);
     for (let i = 0; i < 52; i++)  {
       const val: number = (i % 13) + 2;
       expect(deck.cards[i].value).toEqual(val);
