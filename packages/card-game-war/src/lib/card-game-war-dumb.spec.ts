2656d244ce24fe37b28c8c01280499875074dc28
Ryan Swanson <ryan.s.swanson@gmail.com>
2022-04-04 16:47:23 -0400

    refactor around a better understanding of types

--- packages/card-game-war/src/lib/card-game-war-dumb.spec.ts
+++ packages/card-game-war/src/lib/card-game-war-dumb.spec.ts
@@ -1,21 +1,30 @@
-import { cardGameWarDumb, Deck, Card, Suit, doesMyCardWin } from './card-game-war-dumb';
+import exp = require('constants');
+import { cardGameWarDumb, War, Card, Suit, doesMyCardWin } from './card-game-war-dumb';
 
 describe('the basics', () => {
   it('should work', () => {
     expect(cardGameWarDumb()).toEqual('card-game-war');
   });
   it('should initialize a new 52 card deck without shuffling', () => {
-    const deck = new Deck();
+    const deck = new War();
     expect(deck.cards.length).toBe(52);
     for (let i = 0; i < 52; i++)  {
-      expect(deck.cards[i]).toBeInstanceOf(Card)
       const val: number = (i % 13) + 2;
       expect(deck.cards[i].value).toEqual(val);
     }
   });
+  it('should first draw an ace of spades in an unshuffled deck', () => {
+    const deck: War = new War();
+    const firstCard: Card = deck.draw();
+    expect(firstCard.value).toEqual(14)
+    expect(firstCard.suit).toEqual(Suit.Spades)
+    const secondCard: Card = deck.draw();
+    expect(secondCard.value).toEqual(13)
+    expect(secondCard.suit).toEqual(Suit.Spades)
+  });
   it('should be able to tell you if your card wins or not', () => {
-    const myCard = new Card(Suit["Clubs"],4)
-    const enemyCard = new Card(Suit["Spades"],2)
+    const myCard = { "suit": Suit.Clubs, "value": 4};
+    const enemyCard = { "suit": Suit.Clubs, "value": 2}
     expect(doesMyCardWin(myCard,enemyCard)).toBe(true)
   });
   // it('should tell me my card is bigger')
--- packages/card-game-war/src/lib/card-game-war-dumb.ts
+++ packages/card-game-war/src/lib/card-game-war-dumb.ts
@@ -4,48 +4,61 @@ export enum Suit {
   Hearts,
   Spades,
 }
-type CardType = {
-  // name: string;
-  suit: Suit;
-  value: number;
+export type Card = {
+  name?: string;
+  suit: Suit,
+  value: number,
 }
 
-type DeckType = {
-  cards: Array<CardType>;
-  currentPosition: number
+interface Deck {
+  cards: Array<Card>;
+  draw: () => Card;
+  shuffle: () => void;
 }
-export class Card implements CardType {
-  suit: Suit;
-  value: number;
-  constructor(suit: Suit, value: number) {
-    // this.name = name;
-    this.suit = suit;
-    this.value = value;
-  }
-}
-export class Deck implements DeckType {
+export class War implements Deck {
   cards: Array<Card> = [];
-  currentPosition = 0;
   constructor() {
     let count = 0;
     for (let suit = 0; suit < 4; suit++) {
       for (let value = 2; value <= 14; value++) {
         if (suit == 0)
-          this.cards[count] = new Card(Suit["Clubs"],value);
+          this.cards[count] = { "suit": Suit.Clubs, "value": value};
         if (suit == 1)
-          this.cards[count] = new Card(Suit["Diamonds"],value);
+          this.cards[count] = { "suit": Suit.Diamonds, "value": value};
         if (suit == 2)
-          this.cards[count] = new Card(Suit["Hearts"],value);
+          this.cards[count] = { "suit": Suit.Hearts, "value": value};
         if (suit == 3)
-          this.cards[count] = new Card(Suit["Spades"],value);
+          this.cards[count] = { "suit": Suit.Spades, "value": value};
         count++
       }
     }
+  };
+
+  draw(): Card {
+    const drawCard = this.cards[this.cards.length-1];
+    this.cards.pop()
+    return drawCard;
   }
+
+  shuffle(): void {
+    let currentIndex = this.cards.length,  randomIndex;
+
+      // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [this.cards[currentIndex], this.cards[randomIndex]] = [
+        this.cards[randomIndex], this.cards[currentIndex]];
+    }
+  };
 };
 
 export function doesMyCardWin(myCard: Card, notMyCard: Card): boolean {
-  return myCard["value"] > notMyCard["value"];
+  return myCard.value > notMyCard.value;
 }
 
 export function cardGameWarDumb(): string {
