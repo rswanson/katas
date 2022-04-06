a250d10af04f4e0f197c4079941389f7df8de078
Ryan Swanson <ryan.s.swanson@gmail.com>
2022-04-06 19:45:32 -0400

    initial scaffolding for kata-cli

--- packages/kata-cli/src/main.ts
+++ packages/kata-cli/src/main.ts
@@ -1,16 +1,21 @@
 #!/usr/bin/env node
-import yargs = require('yargs/yargs')
-import { hideBin } from 'yargs/helpers'
+import yargs = require('yargs/yargs');
+import { hideBin } from 'yargs/helpers';
 
 yargs(hideBin(process.argv))
-  .command('deal [numPlayers]', 'deal out a game for numPlayers number of players', (yargs) => {
-    return yargs;
-  }, (argv) => {
-    console.log('done')
-  })
+  .command(
+    'deal [numPlayers]',
+    'deal out a game for numPlayers number of players',
+    (yargs) => {
+      return yargs;
+    },
+    (argv) => {
+      console.log('done');
+    }
+  )
   .option('verbose', {
     alias: 'v',
     type: 'boolean',
-    description: 'Run with verbose logging'
+    description: 'Run with verbose logging',
   })
-  .parse()
+  .parse();
