cbe4f978708d25e7a9056cf9615a372a18efaadd
Ryan Swanson <ryan.s.swanson@gmail.com>
2022-04-06 19:04:29 -0400

    updating kata-cli to an app instead of a lib

--- packages/kata-cli/src/lib/kata-cli.spec.ts
+++
@@ -1,7 +0,0 @@
-import { kataCli } from './kata-cli';
-
-describe('kataCli', () => {
-  it('should work', () => {
-    expect(kataCli()).toEqual('kata-cli');
-  });
-});
--- packages/kata-cli/src/lib/kata-cli.ts
+++
@@ -1,3 +0,0 @@
-export function kataCli(): string {
-  return 'kata-cli';
-}
