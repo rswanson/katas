4bf0e0eae7313e7232a9f3b201c3412ee6c3e2e1
Ryan Swanson <ryan.s.swanson@gmail.com>
2022-04-12 17:28:56 -0400

    adding a somewhat working deal command

--- packages/kata-cli/src/main.ts
+++ packages/kata-cli/src/main.ts
@@ -1,16 +1,36 @@
 #!/usr/bin/env node
 import yargs = require('yargs/yargs');
 import { hideBin } from 'yargs/helpers';
+import { Deck } from '@katas/card-game-war';
 
 yargs(hideBin(process.argv))
   .command({
-    command: 'deal [numPlayers]',
+    command: 'deal [numPlayers: number]',
     describe: 'deal out a game for numPlayers number of players',
     builder: (yargs) => {
       return yargs;
     },
     handler: (argv) => {
-      console.log(argv.numPlayers);
+      const deck = new Deck();
+      deck.shuffle();
+      const hands = deck.deal(1);
+      let suitName: string;
+      switch (hands[0].cards[0].suit) {
+        case 0:
+          suitName = 'Clubs';
+          break;
+        case 1:
+          suitName = 'Diamonds';
+          break;
+        case 2:
+          suitName = 'Hearts';
+          break;
+        case 3:
+          suitName = 'Spades';
+          break;
+      }
+
+      console.log(hands[0].cards[0].value + ' of ' + suitName);
     },
   })
   .command({
